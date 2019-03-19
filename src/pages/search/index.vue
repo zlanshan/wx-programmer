44b09b7ee061706f7862a221cdbe3d48dce19f71
zlanshan <804852539@qq.com>
2019-03-19 21:29:00 +0800

    搜索页面静态布局完成

--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -1,10 +1,45 @@
 <template>
   <view>
+    <!-- 搜索框 -->
     <view class="search">
       <view class="search-input">
         <icon type="search" size="40rpx"></icon>
+         <input type="text" placeholder="请输入你想要的商品"/>
       </view>
-      <view type="button"></view>
+      <button
+        size="mini"
+        type="defult"
+        class="cancel"
+        v-show="false"
+        >
+        取消
+      </button>
+    </view>
+
+    <!-- 历史搜索 -->
+    <view class="history-title">
+      <text>历史搜索</text>
+      <icon
+        type="clear"
+        size="34rpx">
+      </icon>
+    </view>
+    <!-- 历史列表 -->
+    <view class="history-list">
+      <block v-for="(item,index) in [13,2,3,5]" :key="index">
+        <view class="history-list-item">
+         {{item}}
+        </view>
+      </block>
+    </view>
+
+    <!-- 搜索提示 -->
+    <view class="search-tips">
+      <block v-for="(item,index) in [1,3,4,6,83,23]" :key="index">
+      <view class="search-tips-item">
+         {{item}}
+      </view>
+      </block>
     </view>
   </view>
 </template>
@@ -17,10 +52,12 @@ export default {
 
 <style lang="scss">
 .search {
+  display: flex;
   background-color: #eee;
   // height: 100rpx;
   padding: 30rpx 20rpx;
   .search-input {
+    flex:1;
     height: 64rpx;
     font-size: 32rpx;
     color:#666;
@@ -34,6 +71,49 @@ export default {
   .search-input icon {
     margin-left: 30rpx;
     margin-right: 20rpx;
+  }
+  .cancel{
+    margin-left:20rpx;
+    width:160rpx;
+    line-height: 64rpx;
+    background-color: #eee;
+    text-align: center;
+    font-size: 32rpx;
+    padding:0rpx 20rpx;
+  }
+}
+// 历史分区
+.history-title{
+  padding:20rpx;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+// 历史列表
+.history-list{
+  padding:10rpx;
+  .history-list-item{
+    display: inline-block;
+    padding: 0 20rpx;
+    background-color: #eee;
+    line-height: 2;
+    margin:10rpx;
+  }
+}
+// 搜索提示
+.search-tips{
+  position:fixed;
+  left:0;
+  right: 0;
+  top:100rpx;
+  bottom: 0;
+  background-color: #eee;
+  .search-tips-item{
+    border-bottom: 1px solid #ccc;
+    padding:20rpx;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
   }
 }
 </style>
